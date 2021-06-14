e5d300a02afcea74a292fee39785539ef8beabe6
simoneliskases <simon.eliskases@outlook.de>
2021-06-14 12:37:07 +0200

    Added WelcomeScene Manager

--- Assets/MyGame/Scripts/Data.cs
+++ Assets/MyGame/Scripts/Data.cs
@@ -7,4 +7,7 @@ public class Data : ScriptableObject
 {
     public string playerName;
     public int maxSum;
+    public int summandOne;
+    public int summandTwo;
+    public int attempts;
 }
---
+++ Assets/MyGame/Scripts/WelcomeManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WelcomeManager : MonoBehaviour
+{
+    public InputField nameInput;
+    public Data data;
+    
+    public void StartGame()
+    {
+        if(nameInput.text == null || nameInput.text == "")
+        {
+            return;
+        }
+
+        data.maxSum = 10;
+        int sum = Random.Range(2, data.maxSum);
+        data.summandOne = Random.Range(1, sum);
+        data.summandTwo = sum - data.summandOne;
+
+        SceneManager.LoadScene(1);
+    }
+}
