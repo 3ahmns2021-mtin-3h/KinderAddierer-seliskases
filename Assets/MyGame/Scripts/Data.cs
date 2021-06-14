0d83cb60ddfd87fe962bde14103a5405b0e3fce1
simoneliskases <simon.eliskases@outlook.de>
2021-06-14 12:21:11 +0200

    Created Adder Class

---
+++ Assets/MyGame/Scripts/Data.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data")]
+public class Data : ScriptableObject
+{
+    public string playerName;
+    public int maxSum;
+}
